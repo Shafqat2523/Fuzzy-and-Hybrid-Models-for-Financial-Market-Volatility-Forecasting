d19c07a930754b1802be925652e83b14506a0002
Shafqat2523 <shafqat.behal905@gmail.com>
2026-04-14 14:59:31 +0200

    Function in fuzzy_EWMA.R

--- scripts/fuzzy_EWMA.R
+++ scripts/fuzzy_EWMA.R
@@ -1,31 +1,10 @@
 # Functions that runs the FTS-EWMA forecast
-fts <- function(DT=d1,EW=100,nclus=3,lambda=0.94,window=c('rolling','expanding')) {
+fts <- function(EW=100,nclus=3,lambda=0.94,window=c('rolling','expanding')) {
   library(TTR)    # For EWMA
   library(e1071)  # For fuzzy logic
   library(fclust) # For fuzzy clustering
   library(zoo)
-  
-  # Number of observations
-  TT    = dim(DT)[1]
-  # Forecasted values
-  DT$FF = rep(NA,TT)
-  # Dependent variable
-  dep   = all.vars(spec)[1]
-  # Forecast horizon?
-  h                   = as.numeric(substr(dep,start=nchar(dep),stop=nchar(dep)))
-  names(DT)[ncol(DT)] = paste('FTS_',substr(dep,start=2,stop=nchar(dep)),sep='')
-  # Here we store forecasts - preliminary -> later we add to DT
-  prds                = rep(NA,TT)
-  # 'Last' forecast is the average from the initial window
-  prds[EW]            = mean(DT[1:EW,dep],na.rm=T)
-  # Initialize the previous forecast to the mean of the initial window
-  for (w in EW:(TT-1)) {
-    
-    if (window=='rolling') k = w-EW+1 else k = 1
-    DTW <- DT[k:w,dep]
-    DTW <- DTW[complete.cases(DTW)]
-    
-    # Apply Fuzzy C-Means clustering on the rolling window data
+  # Apply Fuzzy C-Means clustering on the rolling window data
     fcm_result          <- cmeans(DTW, centers = nclus)
     cluster_memberships <- fcm_result$membership
     cluster_centroids   <- fcm_result$centers
@@ -49,10 +28,6 @@ fts <- function(DT=d1,EW=100,nclus=3,lambda=0.94,window=c('rolling','expanding')
     
     # Final membership degrees from FCM for the last data point in the window
     final_membership <- cluster_memberships[nrow(cluster_memberships), ]
-    
-    # Transition probabilities from FLR matrix for the latest fuzzy state
-    latest_state <- fuzzy_states[length(fuzzy_states)]
-    transition_probs <- flr_matrix[latest_state, ]
     
     # Adjust membership degrees using transition probabilities 
     # FLR Matrix allows these final membership degrees to be weighted by previous (historical) transition probabilities
